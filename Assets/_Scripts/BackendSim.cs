4453159a876f98bb6235965d89f483b2d4a711e8
densuhi <dk0984459@gmail.com>
2026-05-27 19:24:15 +0300

    Add bird_4

--- Assets/_Scripts/BackendSim.cs
+++ Assets/_Scripts/BackendSim.cs
@@ -5,11 +5,11 @@ public class BackendSim : MonoBehaviour
     public UISwipeDetector detector;
 
     // Список імен птахів — мають збігатися з BirdConfig.birdName в Inspector
-    private string[] birdNames = { "bird_2" };
+    private string[] birdNames = { "bird_4" };
 
     void Start()
     {
         string randomBird = birdNames[Random.Range(0, birdNames.Length)];
-        detector.SetupEgg(randomBird, 15.0f);
+        detector.SetupEgg(randomBird, 5.0f);
     }
 }
