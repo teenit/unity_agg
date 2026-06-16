37dc801d421cc5c0ae4b237841a629ffbb1013bd
Vanuiy <vanya93shevchenko@gmail.com>
2026-06-16 18:35:42 +0300

    All objects disappearence when tapping on the egg

--- Assets/_Scripts/GameCleaner.cs
+++ Assets/_Scripts/GameCleaner.cs
@@ -6,6 +6,8 @@ public class GameCleaner : MonoBehaviour
     public GameObject birdObject;      // Об'єкт із зображенням птаха (Egg)
     public GameObject temperatureUI;   // Весь об'єкт Slider (шкала)
     public GameObject debugTextUI;     // Текст дебагу (якщо треба приховати)
+    public GameObject barLine1;
+    public GameObject barLine2;
 
     // Цю функцію ми викликаємо через UnityEvent (OnBirdClick)
     public void ClearPlayfield()
@@ -13,6 +15,8 @@ public class GameCleaner : MonoBehaviour
         if (birdObject != null) birdObject.SetActive(false);
         if (temperatureUI != null) temperatureUI.SetActive(false);
         if (debugTextUI != null) debugTextUI.SetActive(false);
+        if (barLine1 != null) barLine1.SetActive(false);
+        if (barLine2 != null) barLine2.SetActive(false);
 
         Debug.Log("<color=white>Поле очищено!</color>");
     }
